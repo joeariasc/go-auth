6a7270fb0ac94ff3d13dad6ecb7b772472c63bcb
Joe Arias <jarias@wawand.co>
2025-02-10 09:35:20 -0500

    removing web and mobile fingerprint structs, adding hash method to Base fingerprint struct

--- internal/models/fingerprint.go
+++ internal/models/fingerprint.go
@@ -10,15 +10,6 @@ type BaseFingerprint struct {
 	ClientType ClientType
 	IP         string
 	UserAgent  string
-	RandomSalt string
-}
-
-type WebFingerprint struct {
-	BaseFingerprint
-	ScreenResolution string
-	ColorDepth       string
-	TimeZone         string
-	Language         string
 }
 
 type MobileFingerprint struct {
@@ -29,32 +20,11 @@ type MobileFingerprint struct {
 	IsEmulator    bool
 }
 
-// Hash methods for different fingerprint types
-func (wf *WebFingerprint) Hash() string {
-	data := fmt.Sprintf("%s|%s|%s|%s|%s|%s|%s|%s",
-		wf.ClientType,
-		wf.IP,
-		wf.UserAgent,
-		wf.RandomSalt,
-		wf.ScreenResolution,
-		wf.ColorDepth,
-		wf.TimeZone,
-		wf.Language,
-	)
-	hash := sha256.Sum256([]byte(data))
-	return base64.URLEncoding.EncodeToString(hash[:])
-}
-
-func (mf *MobileFingerprint) Hash() string {
-	data := fmt.Sprintf("%s|%s|%s|%s|%s|%s|%s|%v",
-		mf.ClientType,
-		mf.IP,
-		mf.UserAgent,
-		mf.RandomSalt,
-		mf.DeviceModel,
-		mf.OSVersion,
-		mf.ScreenDensity,
-		mf.IsEmulator,
+func (bf BaseFingerprint) Hash() string {
+	data := fmt.Sprintf("%s|%s|%s",
+		bf.ClientType,
+		bf.IP,
+		bf.UserAgent,
 	)
 	hash := sha256.Sum256([]byte(data))
 	return base64.URLEncoding.EncodeToString(hash[:])
